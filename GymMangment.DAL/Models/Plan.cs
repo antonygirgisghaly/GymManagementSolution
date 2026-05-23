3bdd779d2ddd36f4c26831e81632efe221615a1e
Antony Girgis <antony26429122@gmail.com>
2026-05-23 15:35:10 +0300

    feat: add Trainer Category and Session entities

--- GymMangment.DAL/Models/Plan.cs
+++ GymMangment.DAL/Models/Plan.cs
@@ -1,14 +1,13 @@
-﻿namespace GymMangement.Models
+﻿using GymMangment.DAL.Models;
+
+namespace GymMangement.Models
 {
-    public class Plan
+    public class Plan : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
         public string Description { get; set; }
         public int DuirationDays { get; set; }
         public decimal Price { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
     }
 }
---
+++ GymMangment.DAL/Models/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using GymMangment.DAL.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Models
+{
+    public class Trainer : GymUser
+    {
+        public Specialty Specialty { get; set; }
+        //HireDate = CreatedAt at GymUser
+    }
+}
