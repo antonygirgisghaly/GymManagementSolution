708e76ced01282ec6fd01036ba51688ca5ade373
Antony Girgis <antony26429122@gmail.com>
2026-05-23 09:33:39 +0300

    feat: add GymUser abstract base class with Address owned type

---
+++ GymMangment.DAL/Models/GymUser.cs
@@ -0,0 +1,27 @@
+﻿using GymMangment.DAL.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Models
+{
+    public abstract class GymUser : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public Address Address { get; set; }
+        public Gender Gender { get; set; }
+    }
+    [Owned]
+    public class Address 
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int BuildingNumber { get; set; }
+    }
+}
