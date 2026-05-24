a72e90259a18068da2abda65d202043023682bd6
Antony Girgis <antony26429122@gmail.com>
2026-05-24 19:01:36 +0300

    Feat : Add SessionConfigration

---
+++ GymMangment.DAL/Configrations/SessionConfigration.cs
@@ -0,0 +1,24 @@
+﻿using GymMangment.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Quic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Configrations
+{
+    public class SessionConfigration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.ToTable(tb => 
+            {
+                tb.HasCheckConstraint("SessionCapacityCheck", "Capacity Between 1 and 25");
+                tb.HasCheckConstraint("SessionEndDateCheck", "EndDate > StartDate");
+             });
+        }
+    }
+}
