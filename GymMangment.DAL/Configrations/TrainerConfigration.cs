67b557978fbbb4f73a1b9c93ab7f84bee887baa9
Antony Girgis <antony26429122@gmail.com>
2026-05-24 17:13:42 +0300

    feat: Add TrainerConfigration

---
+++ GymMangment.DAL/Configrations/TrainerConfigration.cs
@@ -0,0 +1,23 @@
+﻿
+using GymMangment.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Configrations
+{
+    public class TrainerConfigration : GymUserConfigration<Trainer> , IEntityTypeConfiguration<Trainer>
+    {
+        public new void Configure(EntityTypeBuilder<Trainer> builder)
+        {
+            builder.Property(a => a.CreatedAt)
+                  .HasColumnName("HireDate")
+                  .HasColumnType("GETDATE()");
+            base.Configure(builder);
+        }
+    }
+}
