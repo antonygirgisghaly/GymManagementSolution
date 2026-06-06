daacfb3fe0f14ff9554b31c469329b4330b5ccda
Antony Girgis <antony26429122@gmail.com>
2026-06-06 09:04:01 +0300

    Fit : Add Health Record Configration

---
+++ GymMangment.DAL/Configrations/HealthRecordConfigration.cs
@@ -0,0 +1,22 @@
+﻿using GymMangment.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Configrations
+{
+    public class HealthRecordConfigration : IEntityTypeConfiguration<HealthRecord>
+    {
+        public void Configure(EntityTypeBuilder<HealthRecord> builder)
+        {
+            builder.Property(b => b.BloodType)
+                   .HasMaxLength(5);
+            builder.Property(b => b.Note)
+                   .HasMaxLength(500);
+        }
+    }
+}
