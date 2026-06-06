38dd937b4028d23392a4e7df142e856b02d3d904
Antony Girgis <antony26429122@gmail.com>
2026-06-06 08:08:24 +0300

    Fit: Add MemberShipConfigration

---
+++ GymMangment.DAL/Configrations/MemberShipConfigration.cs
@@ -0,0 +1,23 @@
+﻿using GymMangment.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Configrations
+{
+    public class MemberShipConfigration : IEntityTypeConfiguration<MemberShip>
+    {
+        public void Configure(EntityTypeBuilder<MemberShip> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.CreatedAt)
+                   .HasColumnName("StartDate")
+                   .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
