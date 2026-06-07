20d1fe1e6224321bc3540d0c5f4cf058e14ceae8
Antony Girgis <antony26429122@gmail.com>
2026-06-07 20:33:35 +0300

    Feat : Add MemberService and IMemberService To BLL

---
+++ GymMangment.BLL/Services/Classes/MemberService.cs
@@ -0,0 +1,38 @@
+﻿using GymMangment.BLL.Services.Interfaces;
+using GymMangment.BLL.ViewModels.MemberViewModels;
+using GymMangment.DAL.Data.Models;
+using GymMangment.DAL.Reposatories.Interfaces;
+using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.BLL.Services.Classes
+{
+    public class MemberService : IMemberService
+    {
+        private IGenaricReposatory<Member> _memberService;
+
+        public MemberService(IGenaricReposatory<Member> memberService) 
+        {
+            _memberService = memberService;
+        }
+        public async Task<IEnumerable<MemberViewModel>> GetAllMembersAsync(CancellationToken ct = default)
+        {
+            var members = await _memberService.GetAllAsync(ct : ct);
+            if (members.Any()) return [] ;
+            var membermodels = members.Select(m => new MemberViewModel()
+            { 
+                Email = m.Email,
+                Phone = m.Phone,
+                Photo = m.Photo,
+                Gender = m.Gender.ToString(),
+                Name = m.Name,
+                Id = m.Id
+            });
+            return membermodels;
+        }
+    }
+}
