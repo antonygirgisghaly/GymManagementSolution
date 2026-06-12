dfce553a4129b5ba4e224ad2a3df24adae6eb09f
Antony Girgis <antony26429122@gmail.com>
2026-06-12 20:44:47 +0300

    feat : Add Data seeding in PlanConfigration

--- GymMangment.DAL/Data/Configrations/PlanConfigration.cs
+++ GymMangment.DAL/Data/Configrations/PlanConfigration.cs
@@ -1,6 +1,7 @@
 ﻿using GymMangment.DAL.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
 
 namespace GymMangment.DAL.Data.Configrations
 {
@@ -18,7 +19,44 @@ namespace GymMangment.DAL.Data.Configrations
             builder.Property(p => p.CreatedAt)
                    .HasDefaultValueSql("GETDATE()");
             builder.ToTable(tb => tb.HasCheckConstraint("PlanDuirationCheck", "DuirationDays between 1 and 365"));
-                
+            builder.HasData(
+        new Plan
+        {
+            Id = 1,
+            Name = "Basic Plan",
+            Price = 300,
+            DuirationDays = 30, 
+            Description = "Access to gym equipment during staffed hours",
+            IsActive = true
+        },
+        new Plan
+        {
+            Id = 2,
+            Name = "Standard Plan",
+            Price = 500,
+            DuirationDays = 60,
+            Description = "Includes gym equipment and 2 group classes per week",
+            IsActive = false 
+        },
+        new Plan
+        {
+            Id = 3,
+            Name = "Premium Plan",
+            Price = 900,
+            DuirationDays = 90,
+            Description = "Unlimited access to equipment, classes, and sauna",
+            IsActive = false 
+        },
+        new Plan
+        {
+            Id = 4,
+            Name = "Annual Plan",
+            Price = 3000,
+            DuirationDays = 360,
+            Description = "Full year access with personal trainer sessions",
+            IsActive = true
+        }
+    );
         }
 
     }
---
+++ GymMangment.DAL/Data/Migrations/20260611200951_PlanDataSeeding.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GymMangment.DAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlanDataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Plan",
+                columns: new[] { "Id", "Description", "DuirationDays", "IsActive", "Name", "Price", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, "Access to gym equipment during staffed hours", 30, true, "Basic Plan", 300m, null },
+                    { 2, "Includes gym equipment and 2 group classes per week", 60, false, "Standard Plan", 500m, null },
+                    { 3, "Unlimited access to equipment, classes, and sauna", 90, false, "Premium Plan", 900m, null },
+                    { 4, "Full year access with personal trainer sessions", 360, true, "Annual Plan", 3000m, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Plan",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Plan",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Plan",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Plan",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
