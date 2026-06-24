15ceb67721dae3f8d769d57ec013edcd5ddf4f71
Antony Girgis <antony26429122@gmail.com>
2026-06-24 14:05:26 +0300

    feat: add identity data seeding for roles and first admin user

---
+++ GymMangment.DAL/Data/DbContexts/IdentityDataSeeding.cs
@@ -0,0 +1,75 @@
+﻿using GymMangment.DAL.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangment.DAL.Data.DbContexts
+{
+    public class IdentityDataSeeding
+    {
+        public static async Task SeedIdentityDataAsync(RoleManager<IdentityRole> roleManager
+            , UserManager<ApplicationUser> userManager
+            , ILogger<IdentityDataSeeding> logger
+            , CancellationToken ct = default)
+        {
+            try
+            {
+                var checkUser = await userManager.Users.AnyAsync();
+                var checkRole = await roleManager.Roles.AnyAsync();
+                if (checkRole && checkUser) return;
+                var roles = new List<IdentityRole>
+            {
+                new IdentityRole("SuperAdmin"),
+                new IdentityRole("Admin")
+            };
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role.Name!))
+                    {
+                        var roleResult = await roleManager.CreateAsync(role);
+                        if (!roleResult.Succeeded)
+                        {
+                            logger.LogError($"Failed to create Role {role.Name} : {string.Join(" ; ", roleResult.Errors.Select(d => d.Description))}");
+                        }
+                        if (!checkUser)
+                        {
+                            var MainAdmin = new ApplicationUser
+                            {
+                                FirstName = "Antony",
+                                LastName = "Girgis",
+                                Email = "antonygirir@gmail.com",
+                                UserName = "Antony Girgis",
+                                PhoneNumber = "01264987530"
+                            };
+                            await userManager.CreateAsync(MainAdmin, "P@ssw0rd");
+                            await userManager.AddToRoleAsync(MainAdmin, "SuperAdmin");
+                            var Admin = new ApplicationUser
+                            {
+                                FirstName = "Andrew",
+                                LastName = "Girgis",
+                                Email = "andrewgirir@gmail.com",
+                                UserName = "Antony Girgis",
+                                PhoneNumber = "01264117530"
+                            };
+                            await userManager.CreateAsync(Admin, "P@ssw0rd");
+                            await userManager.AddToRoleAsync(Admin, "Admin");
+                            logger.LogInformation("Identity Data Seeded");
+                            return;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, "Identity Seeded fail");
+                return;
+            }
+        }
+    }
+}
---
+++ GymMangment.DAL/DataSeeding/GymDataSeeding.cs
@@ -0,0 +1,60 @@
+﻿using GymMangment.DAL.Data.Models;
+using GymMangment.DAL.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace GymMangment.DAL.Data.DataSeeding
+{
+    public static class GymDataSeeding
+    {
+        public static async Task SeedAsync(
+            GymDbContext dbContext,
+            string seedFolderPath,
+            ILogger logger,
+            CancellationToken ct = default)
+        {
+            try
+            {
+                if (!await dbContext.Plan.AnyAsync(ct))
+                {
+                    var plans = LoadDataFromJsonFile<Plan>(
+                        seedFolderPath,
+                        "plans.json");
+
+                    if (plans.Any())
+                    {
+                        dbContext.Plan.AddRange(plans);
+
+                        var result =
+                            await dbContext.SaveChangesAsync();
+
+                        logger.LogInformation(
+                            $"Plans Seeded With Count = {result}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex,
+                    "An Error Occurred While Seeding Data");
+            }
+        }
+
+        private static List<T> LoadDataFromJsonFile<T>(
+            string folderPath,
+            string fileName)
+        {
+            var filePath = Path.Combine(folderPath, fileName);
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException(
+                    $"Seed Data File Not Found : {filePath}");
+
+            var data = File.ReadAllText(filePath);
+
+            return JsonSerializer.Deserialize<List<T>>(data)
+                   ?? new List<T>();
+        }
+    }
+}
