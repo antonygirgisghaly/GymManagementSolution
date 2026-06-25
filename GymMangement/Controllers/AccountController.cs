1a2581a2f35d700c0c9a4afd500e1a30b2338bf7
Antony Girgis <antony26429122@gmail.com>
2026-06-25 22:46:21 +0300

    feat: add AccountController with Login and Logout actions

---
+++ GymMangement/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using GymMangement.Controllers;
+using GymMangment.BLL.ViewModels.AccountViewModel;
+using GymMangment.DAL.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMangement.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<ApplicationUser> _logger;
+
+        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager,ILogger<ApplicationUser> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model,CancellationToken ct)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if(user == null)
+            {
+                ModelState.AddModelError("Invaild Login", "Invalid Email or Password");
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"User {user.UserName} Is Sgined In");
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+            else if(result.IsLockedOut)
+            {
+                _logger.LogWarning($"User {user.UserName} Is Loged Out");
+                ModelState.AddModelError("InvalidLogin", "This Account is Locked Try agin Later");
+                return View(model);
+            }
+            else
+            {
+                ModelState.AddModelError("Invaild Login", "Invalid Email or Password");
+                return View(model);
+            }
+         }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
